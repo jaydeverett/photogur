ec78bc829bca690e8c9bb6023012f0a73aa6a36e
Jay Deverett <jayjdeverett@gmail.com>
2017-07-25 18:40:43 -0400

    add methods for listing pictures more than a month old and pictures from a year

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,8 @@
 class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
+    @pictures_more_than_a_month = Picture.created_before(1.month.ago)
+    @pictures_from_a_year = Picture.pictures_created_in_year(2017)
   end
 
   def show
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,12 @@
 class Picture < ApplicationRecord
+
+  def self.created_before(time)
+  Picture.where("created_at < ?", time)
+  end
+
+  def self.pictures_created_in_year(year)
+    Picture.where("created_at" == year)
+  end
+
+
 end
